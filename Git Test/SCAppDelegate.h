749b9a8ffc51aa3295ed54b0d93361e77236cc81
Szymon Cabaj <Szymon@Patricks-MacBook-Pro.local>
2014-03-11 21:25:37 -0500

    Initial Commit

---
+++ Git Test/SCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SCAppDelegate.h
+//  Git Test
+//
+//  Created by Szymon Cabaj on 3/11/14.
+//  Copyright (c) 2014 Szymon Cabaj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Git Test/SCViewController.m
@@ -0,0 +1,29 @@
+//
+//  SCViewController.m
+//  Git Test
+//
+//  Created by Szymon Cabaj on 3/11/14.
+//  Copyright (c) 2014 Szymon Cabaj. All rights reserved.
+//
+
+#import "SCViewController.h"
+
+@interface SCViewController ()
+
+@end
+
+@implementation SCViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
