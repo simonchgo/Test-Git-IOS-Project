887bacd6a5c7e46adba8419f18699774800880a4
Szymon Cabaj <Szymon@Patricks-MacBook-Pro.local>
2014-03-11 21:52:36 -0500

    added a test class

---
+++ SCTest.h
@@ -0,0 +1,13 @@
+//
+//  SCTest.h
+//  Git Test
+//
+//  Created by Szymon Cabaj on 3/11/14.
+//  Copyright (c) 2014 Szymon Cabaj. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SCTest : NSObject
+
+@end
---
+++ SCTest.m
@@ -0,0 +1,13 @@
+//
+//  SCTest.m
+//  Git Test
+//
+//  Created by Szymon Cabaj on 3/11/14.
+//  Copyright (c) 2014 Szymon Cabaj. All rights reserved.
+//
+
+#import "SCTest.h"
+
+@implementation SCTest
+
+@end
